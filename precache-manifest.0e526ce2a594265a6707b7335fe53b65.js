ee04c2e1000aba211a2b6590ec130e6ba2d7aecf
alirezab7394 <alireza7394@gmail.com>
2019-12-06 01:45:33 +0330

    Updates

---
+++ precache-manifest.0e526ce2a594265a6707b7335fe53b65.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba481a29fcf1e7f5f4da144d90499af7",
+    "url": "/opeqedemo/index.html"
+  },
+  {
+    "revision": "cace44ff9f9670e3d7cd",
+    "url": "/opeqedemo/static/css/main.f65d6069.chunk.css"
+  },
+  {
+    "revision": "190f3b311a6d48de1da5",
+    "url": "/opeqedemo/static/js/2.5db9f727.chunk.js"
+  },
+  {
+    "revision": "cace44ff9f9670e3d7cd",
+    "url": "/opeqedemo/static/js/main.03f15cf0.chunk.js"
+  },
+  {
+    "revision": "7734128d52c491d0bb79",
+    "url": "/opeqedemo/static/js/runtime-main.3bcf81c4.js"
+  },
+  {
+    "revision": "d4396cb23ddbdaca1ae29a1d04748a32",
+    "url": "/opeqedemo/static/media/HomeHeader.d4396cb2.jpg"
+  },
+  {
+    "revision": "d363e53aa98ca7c2c4efa3f134d6f0c2",
+    "url": "/opeqedemo/static/media/Trebuchet MS.d363e53a.ttf"
+  },
+  {
+    "revision": "40faf2752cd703bd957d6b29941d7705",
+    "url": "/opeqedemo/static/media/arrow.40faf275.svg"
+  },
+  {
+    "revision": "aad3376bb6338ef50a421c2ca2a2b668",
+    "url": "/opeqedemo/static/media/logo.aad3376b.svg"
+  },
+  {
+    "revision": "8fd18a54b09ef9241d3c72e8f1fe1f51",
+    "url": "/opeqedemo/static/media/shopBag.8fd18a54.svg"
+  }
+]);
